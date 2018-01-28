5cee2e140f6aac22f11f524357aa37db780d0905
David Kimaru <kimarudg@gmail.com>
2018-01-28 17:03:03 +0300

    Nullables

--- app/src/main/java/com/gadiness/kimaru/ussdhelper/activity/MainActivity.java
+++ app/src/main/java/com/gadiness/kimaru/ussdhelper/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
