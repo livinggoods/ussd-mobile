b09c5fe48e0eca297d6df3ecc83167dd6538e3b2
David Kimaru <kimarudg@gmail.com>
2018-01-28 19:40:21 +0300

    Cloud comms logging

--- app/src/main/java/com/gadiness/kimaru/ussdhelper/other/ApiSyncHelper.java
+++ app/src/main/java/com/gadiness/kimaru/ussdhelper/other/ApiSyncHelper.java
@@ -69,8 +69,16 @@ public class ApiSyncHelper {
                         url.append("/");
                         url.append(appPreferences.getPhoneQueueEndpoint());
                         Log.log(url.toString());
+                        android.util.Log.d(TAG, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                         android.util.Log.d(TAG, url.toString());
-                        new getPhoneQueueFromApi().execute(url.toString());
+                        android.util.Log.d(TAG, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        // since the ques can be more than one, let us retrieve them
+                        List<Queue> selectedQueues = new UssdDbHelper(context).getSelectedQueues();
+                        for (Queue q: selectedQueues){
+                            String validUrl =url.toString()+ "/"+String.valueOf(q.getId());
+                            android.util.Log.d("PHONES", "API URL IS - "+url.toString());
+                            new getPhoneQueueFromApi().execute(validUrl);
+                        }
                     }
                 });
             }
@@ -79,6 +87,7 @@ public class ApiSyncHelper {
     }
 
     public void scheduleUssdUploadTask(){
+        Log.log("scheduleUssdUploadTask()  - scheduleUssdUploadTask - Starting Uploading Messages to the Cloud");
         final Handler handler = new Handler(Looper.getMainLooper());
         Timer timer = new Timer();
         TimerTask uploadTask = new TimerTask() {
@@ -88,12 +97,13 @@ public class ApiSyncHelper {
                     @Override
                     public void run() {
                         String url = new AppPreferences(context).getUssdEndpoint() ;
+                        android.util.Log.d("USSD", "Task ");
                         new postUssdMessagesToApi().execute(url);
                     }
                 });
             }
         };
-        timer.schedule(uploadTask, 0, 60*1000 * 10); //every 10 minutes
+        timer.schedule(uploadTask, 0, 60*1000 * 3); //every 3 minutes
     }
 
     public String getUrlData (String urlString){
@@ -109,18 +119,71 @@ public class ApiSyncHelper {
                     sb.append(line);
                 }
                 stream = sb.toString();
+                Log.log(TAG+stream);
+                android.util.Log.d("getUrlData", stream);
                 urlConnection.disconnect();
             }else{
+                android.util.Log.d("getUrlData", "Status not 200");
                 Log.log(TAG+" STATUS IS NOT 200 ");
             }
         } catch (MalformedURLException e){
+            android.util.Log.d("getUrlData", "Malformed URL");
             Log.log(TAG+" MALFORMED URL ");
             Log.log(TAG+e.getMessage());
         }catch(IOException e){
+            android.util.Log.d("getUrlData", "Exception");
             Log.log(TAG+" IO EXCEPTION");
             Log.log(TAG+e.getMessage());
         }
         return stream;
+    }
+
+    private class UpdateCloudSelectedQueue extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings){
+            String postResults = null;
+            try {
+                StringBuilder urlString = new StringBuilder();
+                AppPreferences appPreferences = new AppPreferences(context);
+                urlString.append(appPreferences.getApiServer());
+                urlString.append("/api/");
+                urlString.append(appPreferences.getApiVersion());
+                urlString.append("/");
+                urlString.append("selectedqueues");
+                UssdDbHelper ussdDbHelper = new UssdDbHelper(context);
+                postResults = postJsonToUrl(
+                        ussdDbHelper.getSelectedQueuesAsJson(),
+                        UssdDbHelper.UPSTREAM_QUEUE_JSON_ROOT,
+                        urlString.toString()
+                );
+                Log.log(TAG +"  -  "+ postResults);
+            } catch (MalformedURLException e){
+                android.util.Log.d("USSD", "MALFORMED URL "+e.getMessage());
+                Log.log("USSD"+" MALFORMED URL ");
+                Log.log("USSD"+e.getMessage());
+            }catch(IOException e){
+                android.util.Log.d("USSD", " IO EXCEPTION "+e.getMessage());
+                Log.log(TAG+" IO EXCEPTION");
+                Log.log(TAG+e.getMessage());
+            }catch (Exception e) {
+                android.util.Log.d("USSD", "EXCEPTION "+e.getMessage());
+                postResults = null;
+                android.util.Log.d("LGUSD", e.getMessage());
+            }
+            return postResults;
+        }
+    }
+
+    public void notifyCloudSelectedQueues(){
+        Log.log("scheduleUssdUploadTask()  - scheduleUssdUploadTask - Starting Uploading Messages to the Cloud");
+        StringBuilder urlString = new StringBuilder();
+        AppPreferences appPreferences = new AppPreferences(context);
+        urlString.append(appPreferences.getApiServer());
+        urlString.append("/api/");
+        urlString.append(appPreferences.getApiVersion());
+        urlString.append("/");
+        urlString.append("selectedqueues");
+        new UpdateCloudSelectedQueue().execute(urlString.toString());
     }
 
     /**
@@ -134,8 +197,15 @@ public class ApiSyncHelper {
     private String postJsonToUrl(JSONObject json, String JsonRoot, String url) throws Exception {
         //  get the server URL
         AsyncHttpPost p = new AsyncHttpPost(url);
+        android.util.Log.d("USSD", "postToUrl");
+        android.util.Log.d("USSD", json.toString());
+        android.util.Log.d("USSD", url);
         p.setBody(new JSONObjectBody(json));
-        JSONObject ret = AsyncHttpClient.getDefaultInstance().executeJSONObject(p, null).get();
+        JSONObject ret;
+        ret = AsyncHttpClient.getDefaultInstance().executeJSONObject(p, null).get();
+        android.util.Log.d("USSD","===============+RESULTS++++++++++++++++");
+        android.util.Log.d("USSD", ret.toString());
+        android.util.Log.d("USSD","===============+RESULTS++++++++++++++++");
         return ret.getString(JsonRoot);
     }
 
@@ -143,12 +213,15 @@ public class ApiSyncHelper {
         protected String doInBackground(String... strings){
             String stream = null;
             String urlString = strings[0];
+            android.util.Log.d("PHONE", "+++++++++++++++++++++++++++++++++++++++++");
+            android.util.Log.d("PHONE", urlString);
+            android.util.Log.d("PHONE", "}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}");
             stream = getUrlData(urlString);
             if (stream != null){
                 try{
                     JSONObject reader = new JSONObject(stream);
-                    Log.log(TAG + " getPhoneQueueFromApi() - Creating a phone queue record");
-                    android.util.Log.d(TAG, " getPhoneQueueFromApi() - Creating a phone queue record");
+                    Log.log(TAG + " getPhoneQueueFromApi() - Starting to create a phone queue record");
+                    android.util.Log.d(TAG, " getPhoneQueueFromApi() - Starting to create a phone queue record");
                     JSONArray recs = reader.getJSONArray(new AppPreferences(context).getPhoneJsonRoot());
                     for (int x = 0; x < recs.length(); x++){
                         Log.log(TAG + " getPhoneQueueFromApi() - Creating a phone queue record");
@@ -168,18 +241,133 @@ public class ApiSyncHelper {
     }
 
     private class postUssdMessagesToApi extends AsyncTask<String, Void, String> {
+        @Override
         protected String doInBackground(String... strings){
-            String postResults;
+            android.util.Log.d("USSD", " Async Task");
+            String postResults = null;
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
             try {
+                android.util.Log.d("USSD", "--=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-==--=-==-=-=-=-=-=-=-=-=-");
+                android.util.Log.d("USSD", new UssdDbHelper(context).getMessagesToSyncJson().toString());
+                android.util.Log.d("USSD", "--=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-==--=-==-=-=-=-=-=-=-=-=-");
+
+
+
+                StringBuilder urlString = new StringBuilder();
+                AppPreferences appPreferences = new AppPreferences(context);
+
+
+                urlString.append(appPreferences.getApiServer());
+                urlString.append("/api/");
+                urlString.append(appPreferences.getApiVersion());
+                urlString.append("/");
+                urlString.append(appPreferences.getUssdEndpoint());
+                UssdDbHelper ussdDbHelper = new UssdDbHelper(context);
+                Log.log ("--=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-==--=-==-=-=-=-=-=-=-=-=-");
+                Log.log(ussdDbHelper.getMessagesToSyncJson().toString());
+                Log.log("--=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-==--=-==-=-=-=-=-=-=-=-=-");
                 postResults = postJsonToUrl(
-                        new UssdDbHelper(context).getMessagesToSyncJson(),
+                        ussdDbHelper.getMessagesToSyncJson(),
                         UssdDbHelper.USSD_JSON_ROOT,
-                        new AppPreferences(context).getUssdEndpoint()
+                        urlString.toString()
                 );
-            } catch (Exception e){
+                Log.log(TAG +"  -  "+ postResults);
+                // process the messages
+                if (postResults != null){
+                    try{
+                        JSONObject results = new JSONObject(postResults);
+                        JSONArray recs = results.getJSONArray(new AppPreferences(context).getPhoneJsonRoot());
+                        for (int x = 0; x < recs.length(); x++){
+                            // since the message has been uploaded, let us delete it
+                            ussdDbHelper.deleteUssdMessage(ussdDbHelper
+                                    .ussdMessageFromJson(recs.getJSONObject(x)));
+                        }
+                    } catch (JSONException e){
+                        Log.log(TAG + " ERROR getPhoneQueueFromApi() - "+ e.getMessage());
+                        android.util.Log.d(TAG,  " ERROR getPhoneQueueFromApi() - "+ e.getMessage());
+                    }
+                }else{
+                    Log.log(TAG + " ERROR getPhoneQueueFromApi() - No Stream data received");
+                    android.util.Log.d(TAG,  " ERROR getPhoneQueueFromApi() - No Stream data received");
+                }
+
+
+
+            } catch (MalformedURLException e){
+                android.util.Log.d("USSD", "MALFORMED URL "+e.getMessage());
+                Log.log("USSD"+" MALFORMED URL ");
+                Log.log("USSD"+e.getMessage());
+            }catch(IOException e){
+                android.util.Log.d("USSD", " IO EXCEPTION "+e.getMessage());
+                Log.log(TAG+" IO EXCEPTION");
+                Log.log(TAG+e.getMessage());
+            }catch (Exception e) {
+                android.util.Log.d("USSD", "EXCEPTION "+e.getMessage());
                 postResults = null;
+                android.util.Log.d("LGUSD", e.getMessage());
             }
             return postResults;
+        }
+    }
+
+    // GET THE QUEUES
+    public void getQueuesTask(){
+        final Handler handler = new Handler(Looper.getMainLooper());
+        Timer timer = new Timer();
+        TimerTask getQueueTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        AppPreferences appPreferences = new AppPreferences(context);
+                        StringBuilder url = new StringBuilder();
+                        url.append(appPreferences.getApiServer());
+                        url.append("/api/");
+                        url.append(appPreferences.getApiVersion());
+                        url.append("/");
+                        url.append(appPreferences.getQueueEndPoint());
+                        Log.log(url.toString());
+                        new getQueuesFromApi().execute(url.toString());
+                    }
+                });
+            }
+        };
+        timer.schedule(getQueueTask, 0, 60*1000 / 3); //every 20 seconds
+    }
+
+    private class getQueuesFromApi extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... strings){
+            String stream = null;
+            String urlString = strings[0];
+            android.util.Log.d("getQueuesFromApi", "pppppppppppppppppppppppppppppppppppppppp");
+            android.util.Log.d("getQueuesFromApiUrl", urlString);
+            android.util.Log.d("getQueuesFromApi", "pppppppppppppppppppppppppppppppppppppppp");
+            stream = getUrlData(urlString);
+            if (stream != null){
+                android.util.Log.d("getQueuesFromApi", "trying to sync the data");
+                android.util.Log.d("getQueuesFromApi", "**************************");
+                try{
+                    JSONObject reader = new JSONObject(stream);
+                    Log.log(TAG + " getQueuesFromApi() - Starting to create a queues record");
+                    JSONArray recs = reader.getJSONArray(new AppPreferences(context).getQueueJsonRoot());
+                    for (int x = 0; x < recs.length(); x++){
+                        Log.log(TAG + " getQueuesFromApi() - Creating a queue record");
+                        android.util.Log.d(TAG, " getQueuesFromApi() - Creating a phone queue record");
+                        new UssdDbHelper(context).queueFromJson(recs.getJSONObject(x));
+                    }
+                } catch (JSONException e){
+                    Log.log("DATA" + " ERROR getQueuesFromApi() - "+ e.getMessage());
+                    android.util.Log.d("DATAERR",  "==+++++++++++===========+++++++++++++====+++++++======+++==");
+                    android.util.Log.d("DATAERR",  " ERROR getQueuesFromApi() - "+ e.getMessage());
+                    android.util.Log.d("DATAERR",  "==+++++++++++===========+++++++++++++====+++++++======+++==");
+                }
+            }else{
+                android.util.Log.d("getQueuesFromApi", "no stream data");
+                android.util.Log.d("getQueuesFromApi", "**************************");
+            }
+            return  stream;
         }
     }
 }
